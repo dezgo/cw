3b126474325b7ef61b2b062d50bf17f3fda35604
Derek Gillett <github@derekgillett.com>
2016-05-31 08:16:54 +0000

    favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,9 +4,10 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
     <title>Computer Whiz - Canberra</title>
-
+    <link rel="icon"
+          type="image/png"
+          href="/images/favicon.png">
     <link href="/css/all.css" rel="stylesheet">
     <script language="javascript" src="/js/all.js"></script>
 
